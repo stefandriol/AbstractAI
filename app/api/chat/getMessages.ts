c719ddbe438c80003827c27f4c03d4d7bafe6134
WormholeSeeker <stefandriol@gmail.com>
2023-08-29 17:06:32 +0200

    Modified  prompt for proper formatting

--- app/api/chat/getMessages.ts
+++ app/api/chat/getMessages.ts
@@ -31,7 +31,8 @@ const getMessages = async (
         .slice(0, 20)
         .map(({ id, ...rest }) => rest);
     // debugging order again:
-    // console.log("Modified papers order:", papersForPrompt);
+    //console.log("Modified papers order:", papersForPrompt);
+    //console.log("Modified papers stringified:", JSON.stringify(papersForPrompt));
 
     const messages: Message[] = [];
 
@@ -76,8 +77,19 @@ const getMessages = async (
 
         messages.push({
         role: ChatCompletionRequestMessageRoleEnum.User,
-        content: `For each paper, format the output using 4 paragraphs separated by a single line break: title, authors, summary, keywords. Split papers by a double line break.`,
+        content: `For each paper, format the output using 4 paragraphs separated by a single line break: title, authors, summary, keywords. Split papers by a double line break. 
+        Namely: insert_title_here\ninsert_authors_here\ninsert_summary_here\nkeywords:insert_keywords_here\n\n`,
     });
+
+        /*
+        messages.push({
+        role: ChatCompletionRequestMessageRoleEnum.User,
+        content: `For example, given the paper: 
+        {"title":"Higher Derivative Sigma Models","authors":"John F. Donoghue, Gabriel Menezes","abstract":"We explore the nature of running couplings in the higher derivative linear and nonlinear sigma models and show that the results in dimensional regularization for the physical running couplings do not always match the values quoted in the literature. Heat kernel methods identify divergences correctly, but not all of these divergences are related to physical running couplings. Likewise the running found using the Functional Renormalization Group does not always appear as running couplings in physical processes, even for the case of logarithmic running. The basic coupling of the higher derivative SU(N) nonlinear sigma model does not run at all at one loop, in contrast to published claims for asymptotic freedom. At one loop we describe how to properly identify the physical running couplings in these theories, and provide revised numbers for the higher derivative nonlinear sigma model."},
+        the output format must be:
+        Higher Derivative Sigma Models\nJohn F. Donoghue, Gabriel Menezes\nDimensional regularization for the physical running couplings in higher derivative sigma models does not always agree with existing literature. At one loop, we describe how to correctly identify the physical running couplings, and provide a revised result.\nKeywords: higher derivative sigma models, running couplings, asymptotic freedom.\n\n`,
+    });
+         */
 
     return messages;
 };
